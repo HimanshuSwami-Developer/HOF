56b693e43ec3bfbfbc85bbf8f56f893961115c6b
unknown <himanshuswami2810@gmail.com>
2025-01-27 13:03:16 +0530

    email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
   setLoading(true);
 
   try {
-    const response = await fetch("http://localhost:5000/send-email", {
+    const response = await fetch("https://hof-backend.onrender.com/send-email", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
